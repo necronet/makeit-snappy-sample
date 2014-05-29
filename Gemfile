3293de5db3ec918d08d5b94385e7101d6e11d0b5
Jose Ayerdis <joseayerdis@gmail.com>
2014-05-29 00:39:07 -0600

    Configure to make it work with postgresql in heroku

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'will_paginate'
 gem 'bcrypt'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140529051344) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: true do |t|
     t.integer  "user_id"
     t.integer  "question_id"
@@ -21,8 +24,8 @@ ActiveRecord::Schema.define(version: 20140529051344) do
     t.datetime "updated_at"
   end
 
-  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
-  add_index "answers", ["user_id"], name: "index_answers_on_user_id"
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id", using: :btree
 
   create_table "questions", force: true do |t|
     t.integer  "user_id"
@@ -32,7 +35,7 @@ ActiveRecord::Schema.define(version: 20140529051344) do
     t.datetime "updated_at"
   end
 
-  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -41,6 +44,6 @@ ActiveRecord::Schema.define(version: 20140529051344) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
